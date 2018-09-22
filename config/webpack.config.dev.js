c3868572b7235a7f17d0ed8bd6b5a8bf2acf737c
crifff <hoshina85@gmail.com>
2018-09-23 00:20:57 +0900

    sample

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -201,6 +201,14 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.scss$/,
+            use:[
+              require.resolve('style-loader'),
+              require.resolve('css-loader'),
+              require.resolve('sass-loader'),
+            ]
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,121 @@
 import * as React from 'react';
 
-import {Col, Grid, MainContainer,Row} from "@sketchpixy/rubix";
-import './App.css';
+import {
+  Button,
+  Checkbox,
+  Col,
+  ControlLabel,
+  Form,
+  FormControl,
+  FormGroup,
+  Grid,
+  HelpBlock,
+  MainContainer,
+  PageHeader,
+  Panel,
+  PanelBody,
+  PanelContainer,
+  PanelHeader,
+  Radio,
+  Row,
+} from "@sketchpixy/rubix";
+
+import './App.scss';
 
 class App extends React.Component {
   public render() {
     return (
       <MainContainer {...this.props}>
-          <div id="body">
-              <Grid>
-                  <Row>
-                      <Col xs={12}>
-                          hoge
-                      </Col>
-                  </Row>
-              </Grid>
-          </div>
+        <PageHeader>Example page header <small>Subtext for header</small></PageHeader>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <PanelContainer>
+                <Panel>
+                  <PanelHeader className="bg-green">
+                    <Grid>
+                      <Row>
+                        <Col xs={12} className='fg-white'>
+                          <h4>Panel Body + Header without Panel Footer</h4>
+                          <h6>Mini Heading</h6>
+                        </Col>
+                      </Row>
+                    </Grid>
+                  </PanelHeader>
+                  <PanelBody style={{ padding: 0 }}>
+                    <Grid>
+                      <Row>
+                        <Col xs={12}>
+                          <Form>
+                            <FormGroup controlId="formControlsEmail">
+                              <ControlLabel>Email address</ControlLabel>
+                              <FormControl type="email" placeholder="Enter email" />
+                            </FormGroup>
+                            <FormGroup controlId="formControlsPassword">
+                              <ControlLabel>Password</ControlLabel>
+                              <FormControl type="password" />
+                            </FormGroup>
+                            <FormGroup controlId="formControlsFile">
+                              <ControlLabel>File</ControlLabel>
+                              <FormControl type="file" />
+                              <HelpBlock>Example block-level help text here.</HelpBlock>
+                            </FormGroup>
+
+                            <Checkbox checked={true} readOnly={true}>Checkbox</Checkbox>
+                            <Radio checked={true} readOnly={true}>Radio</Radio>
+
+                            <FormGroup>
+                              <Checkbox inline={true}>1</Checkbox>
+                              <Checkbox inline={true}>2</Checkbox>
+                              <Checkbox inline={true}>3</Checkbox>
+                            </FormGroup>
+
+                            <FormGroup>
+                              <Radio inline={true}>1</Radio>
+                              <Radio inline={true}>2</Radio>
+                              <Radio inline={true}>3</Radio>
+                            </FormGroup>
+
+                            <FormGroup controlId="formControlsSelect">
+                              <ControlLabel>Select</ControlLabel>
+                              <FormControl componentClass="select" placeholder="select">
+                                <option value="select">select</option>
+                                <option value="other">...</option>
+                              </FormControl>
+                            </FormGroup>
+
+                            <FormGroup controlId="formControlsSelectMultiple">
+                              <ControlLabel>Multiple select</ControlLabel>
+                              <FormControl componentClass="select" multiple={true}>
+                                <option value="select">select (multiple)</option>
+                                <option value="other">...</option>
+                              </FormControl>
+                            </FormGroup>
+
+                            <FormGroup controlId="formControlsTextarea">
+                              <ControlLabel>Textarea</ControlLabel>
+                              <FormControl componentClass="textarea" placeholder="textarea" />
+                            </FormGroup>
+
+                            <FormGroup>
+                              <ControlLabel>Static text</ControlLabel>
+                              <FormControl.Static>email@example.com</FormControl.Static>
+                            </FormGroup>
+
+                            <FormGroup>
+                              <Button type="submit" bsStyle="primary">Submit</Button>
+                            </FormGroup>
+
+                          </Form>
+                        </Col>
+                      </Row>
+                    </Grid>
+                  </PanelBody>
+                </Panel>
+              </PanelContainer>
+            </Col>
+          </Row>
+        </Grid>
       </MainContainer>
     );
   }
